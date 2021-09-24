351d1990e70e77cbc6aa57e1728cb471bfd86bc6
brayanovalle16 <brayanovalle2@outlook.com>
2021-09-24 11:36:27 -0500

    Cambio de front

--- ProyectoCiclo3.App.Frontend/Startup.cs
+++ ProyectoCiclo3.App.Frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 
 namespace ProyectoCiclo3.App.Frontend
 {
