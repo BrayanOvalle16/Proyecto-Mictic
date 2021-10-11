b4cb1a95b7bcbf24cf7523e5b89550629549c8b0
brayanovalle16 <brayanovalle2@outlook.com>
2021-10-11 18:32:45 -0500

    Creacion2

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
  
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
     public class FormEstacionesModel : PageModel
     {
+ 
+        private readonly RepositorioEstaciones RepositorioEstaciones;
+        [BindProperty]
+        public Estaciones Estacion {get;set;}
+ 
+        public FormEstacionesModel(RepositorioEstaciones RepositorioEstaciones)
+       {
+            this.RepositorioEstaciones=RepositorioEstaciones;
+       }
+ 
         public void OnGet()
         {
  
+        }
+ 
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }            
+            Estacion = RepositorioEstaciones.Create(Estacion);            
+            return RedirectToPage("./List");
         }
     }
 }
